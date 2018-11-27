28c632054398eb9654308517187a17fd30f30f5b
bischmlb <mathiasbischo@gmail.com>
2018-11-27 15:35:51 +0100

    -||-

--- app/src/main/java/com/example/bisch/csgo_smokes/TODO.java
+++ app/src/main/java/com/example/bisch/csgo_smokes/TODO.java
@@ -20,14 +20,19 @@ public class TODO {
 
 
     /*TODO Mirage:
-    TODO: Make videos for all featured smokes -
+    (DONE): Make videos for all featured smokes -
         (DONE) MID
         (DONE) A
         (DONE) B
-    TODO: Determine and enhance layout on video activities
-    TODO: Find new design for slider bar
-    TODO: Find background for mirage themed activities (maybe mirage loading screen)
-    TODO: Tips for how the regarding smoke can be used in different situations?
+    (DONE): Determine and enhance layout on video activities
+    (DONE): Find new design for slider bar
+    (DONE): Find background for mirage themed activities (maybe mirage loading screen)
+    (DONE): // NO: Tips for how the regarding smoke can be used in different situations?
+    TODO: Finish mirage_stairs smoke
+    TODO:
+
+
+    TODO: (Find new smokes icon for map overlook (maybe) )
 
     TODO: Finish mirage as a base before starting on new maps
     */
